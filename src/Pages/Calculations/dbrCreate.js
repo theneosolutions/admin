776c3fa64f054b216c735afa92e28648618c4108
MZainMalik <149575690+MZainMalik@users.noreply.github.com>
2024-03-16 15:22:37 +0500

    update

---
+++ src/Pages/Calculations/dbrCreate.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { useState } from "react";
+import { Button } from "Components";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import * as action from "Services/redux/reducer";
+
+function CreateUser({ setModelOpen }) {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    CreateNewUser();
+  }
+
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function CreateNewUser() {
+    if (email && username && firstName) {
+      dispatch({
+        type: "Add_NEW_USER",
+        payload: {
+          firstName: firstName,
+          username: username,
+          email: email,
+          password: password,
+        },
+      });
+    } else {
+      dispatch(
+        action.Message({
+          message: "All Fields Required!",
+          open: true,
+          error: true,
+        })
+      );
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="items-center justify-center flex flex-col"
+    >
+      <div className=" bg-white rounded shadow-sm  rtl:space-x-reverse flex flex-col lg:flex-row   w-full lg:w-max lg:space-x-20 lg:px-20 px-4 py-5 ">
+        <div className="flex flex-col ">
+          <div className="flex flex-col w-full">
+            <a className="text-sm text-gray-700">Income Bracket</a>
+            <div className=" justify-between flex flex-row items-center border-gray-300 border rounded-md px-1.5 py-1.5 outline-none mt-2 w-full">
+              <input
+                type="number"
+                // value={value}
+                // onChange={(e) => onChange(e.target.value)}
+                className="bg-gray-100 rounded-md px-3 py-1 outline-none w-5/12	"
+              />
+              <a className="text-sm">TO</a>
+              <input
+                type="number"
+                // value={value}
+                // onChange={(e) => onChange(e.target.value)}
+                className="bg-gray-100 rounded-md px-3 py-1 outline-none w-5/12	"
+              />
+            </div>
+          </div>
+          <div className=" w-full  flex md:flex-row flex-col md:space-x-20 mt-5 rtl:space-x-reverse">
+            <div className=" md:w-1/2 w-full space-y-5">
+              <InputField
+                id="firstName"
+                heading={t("Product Level")}
+                value={firstName}
+                onChange={(e) => setFirstName(e)}
+              />
+              <InputField
+                heading={t("Customer DBR")}
+                value={email}
+                onChange={(e) => setEmail(e)}
+              />
+            </div>
+            <div className="md:w-1/2 w-full md:mt-0 mt-3 space-y-5">
+              <InputField
+                heading={t("GDBR (Without MTG)")}
+                value={username}
+                onChange={(e) => setUserName(e)}
+              />
+              <InputField
+                heading={t("GDBR (Include MTG)")}
+                value={password}
+                onChange={(e) => setPassword(e)}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-end mt-10 px-14">
+            <Button
+              type="submit"
+              buttonValue={t("Submit")}
+              buttonStyle="px-20 py-2 w-full "
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+export default CreateUser;
+
+function InputField({ heading, value, onChange, type }) {
+  return (
+    <div className="flex flex-col w-full">
+      <a className="text-sm text-gray-700">{heading}</a>
+
+      <input
+        type={type || "text"}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="border-gray-300 border rounded-md px-3 py-1.5 outline-none mt-2 w-full"
+      />
+    </div>
+  );
+}
--- src/Pages/Calculations/index.js
+++ src/Pages/Calculations/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import CardMain from "../../Components/Cards/main";
 import Edit from "../../Assets/Images/edit.svg";
@@ -9,6 +9,8 @@ import { Alert, Snackbar } from "@mui/material";
 import { useEffect } from "react";
 import withAuthorization from "../../constants/authorization";
 import { ROLES } from "../../constants/roles";
+import CreateDBR from "./dbrCreate";
+
 function Calculations() {
   const dispatch = useDispatch();
   const { t } = useTranslation();
@@ -17,6 +19,9 @@ function Calculations() {
   const message = useSelector((state) => state.message);
   const open = useSelector((state) => state.open);
   const error = useSelector((state) => state.error);
+
+  const [modelOpen, setModelOpen] = useState(false);
+
   const handleClose = () => {
     dispatch(action.Message({ open: false }));
   };
@@ -29,13 +34,18 @@ function Calculations() {
       type: "GET_ALL_USERS_ALL",
     });
   }
-
+  function reset() {
+    setModelOpen(false);
+  }
   return (
     <div className="py-5">
       <CardMain
         width="w-full"
         heading={t("DBR Guidlines")}
         iconStyle="text-3xl text-primary"
+        showButton={true}
+        buttonValue={"Add DBR"}
+        onButtonClick={() => setModelOpen(true)}
       >
         <div className="overflow-x-auto relative  mt-4">
           <table className="w-full whitespace-nowrap  text-sm text-left text-gray-500 dark:text-gray-400">
@@ -99,6 +109,12 @@ function Calculations() {
           {message}
         </Alert>
       </Snackbar>
+
+      {modelOpen ? (
+        <Model setModelOpen={(e) => setModelOpen(e)} reset={() => reset()}>
+          <CreateDBR setModelOpen={(e) => setModelOpen(e)} />
+        </Model>
+      ) : null}
     </div>
   );
 }
@@ -106,6 +122,46 @@ export default withAuthorization(Calculations, [
   ROLES.ADMIN,
   ROLES.UNDER_WRITER,
 ]);
+function Model({ children, reset }) {
+  return (
+    <div
+      id="default-modal"
+      tabIndex="-1"
+      className="bg-gray-200 bg-opacity-30 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+    >
+      <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+            Add DBR
+          </h3>
+          <button
+            type="button"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            onClick={() => reset()}
+          >
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 const data = [
   {
