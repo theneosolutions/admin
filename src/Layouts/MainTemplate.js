b51342255f1b3b1cc774abf5039ee75e4708aded
MZainMalik <149575690+MZainMalik@users.noreply.github.com>
2024-05-03 18:54:29 +0500

    authentication token

--- src/Layouts/MainTemplate.js
+++ src/Layouts/MainTemplate.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import WaveAnimation from "Components/Loading"; // Adjust the path based on your file structure
 import { useSelector, useDispatch } from "react-redux";
 import * as action from "../Services/redux/reducer";
-import { CheckQuestionStatusInScreen, CheckToken } from "Services/OtherApis";
 
 function MainTemplate({ children }) {
   const dispatch = useDispatch();
@@ -14,47 +13,17 @@ function MainTemplate({ children }) {
     if (storage) {
       const user = JSON.parse(storage);
       if (user?.islogin) {
-        CheckTokenfunction(user?.data);
-      }
-    }
-  }, []);
-
-  function CheckTokenfunction(user) {
-    console.log("user");
-    CheckToken(user?.user?.idNumber, user?.token).then((response) => {
-      console.log("Response,response", response?.message);
-      if (response?.message === "Token Expried") {
-        Logout(user?.user?.idNumber);
-      } else {
         dispatch(
           action.Auth({
-            islogin: true,
-            user: user,
-            role: user?.roles[0],
-            token: user?.token,
+            islogin: user?.islogin,
+            user: user?.data,
+            role: user?.data?.roles[0],
+            token: user?.data?.token,
           })
         );
       }
-    });
-  }
-  function Logout(id) {
-    dispatch(
-      action.Auth({
-        islogin: false,
-        user: null,
-        role: null,
-        token: null,
-      })
-    );
-
-    localStorage.removeItem("user");
-
-    dispatch({
-      type: "LOGOUT_USER",
-      payload: id,
-    });
-    window.location.href = "/login";
-  }
+    }
+  }, []);
 
   return (
     <div>
