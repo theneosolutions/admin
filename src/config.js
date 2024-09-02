36b3c7fb16708e2e5ca653d78c1aba9ca625b61c
Zain Malik <149575690+MZainMalik@users.noreply.github.com>
2024-09-02 15:52:11 +0500

    dummy

--- src/config.js
+++ src/config.js
@@ -1,18 +1,25 @@
-const ENV = process.env.REACT_APP_ENV || "production";
+// const ENV = process.env.REACT_APP_ENV || "dev";
+
+// const config = {
+//   development: {
+//     API_URL: "https://seulah.com",
+//     FEATURE_FLAG: true,
+//   },
+//   production: {
+//     API_URL: "https://seulah.sa",
+//     FEATURE_FLAG: false,
+//   },
+//   dev: {
+//     API_URL: "https://seulah.dev",
+//     FEATURE_FLAG: true,
+//   },
+// };
+
+// export default config[ENV];
 
 const config = {
-  development: {
-    API_URL: "https://seulah.com",
-    FEATURE_FLAG: true,
-  },
-  production: {
-    API_URL: "https://seulah.sa",
-    FEATURE_FLAG: false,
-  },
-  localhost: {
-    API_URL: "http://localhost:3000",
-    FEATURE_FLAG: true,
-  },
+  API_URL: process.env.REACT_APP_API_URL,
+  FEATURE_FLAG: process.env.REACT_APP_FEATURE_FLAG === "true",
 };
 
-export default config[ENV];
+export default config;
