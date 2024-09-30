5c9050493ddd3bf522b2f3945b7548a2d82dc2b9
Zain Malik <149575690+MZainMalik@users.noreply.github.com>
2024-09-30 13:37:37 +0500

    changes
    chnages

--- src/constants/authorization.js
+++ src/constants/authorization.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import appRoutes from "Routes/appRoutes";
 // This HOC checks if the user is authorized to access the component
 const withAuthorization = (WrappedComponent, code) => {
   return () => {
     const roles = useSelector((state) => state?.role);
-
+    console.log("ROLESS", roles);
+    console.log("routes", useLocation().pathname, appRoutes);
     useEffect(() => {
       const storage = localStorage.getItem("user");
 
@@ -16,13 +19,14 @@ const withAuthorization = (WrappedComponent, code) => {
         }
       }
     }, []);
+    // const
     const hasPermission = roles?.permissions.some((item) =>
       item.subMenus.some((subMenu) => subMenu.code === code)
     );
-
+    console.log("helooooo permission", hasPermission);
     // If no matching code is found, return null (or a fallback component)
     if (!hasPermission) {
-      return null; // Or you could return a <NotFound /> component or redirect
+      return <NotAllowed />; // Or you could return a <NotFound /> component or redirect
     }
 
     // If the user has the correct permission, render the WrappedComponent
@@ -31,3 +35,11 @@ const withAuthorization = (WrappedComponent, code) => {
 };
 
 export default withAuthorization;
+
+function NotAllowed() {
+  return (
+    <div>
+      <a>Not Allowed</a>
+    </div>
+  );
+}
